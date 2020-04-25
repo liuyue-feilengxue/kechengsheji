8c84dd227ab49336417848a73762047d2db99567
liuyue-feilengxue <62792199+liuyue-feilengxue@users.noreply.github.com>
2020-04-25 12:08:08 +0800

    vcard导入导出写完
    Signed-off-by: liuyue-feilengxue <62792199+liuyue-feilengxue@users.noreply.github.com>

--- src/address_book/AddressUtil.java
+++ src/address_book/AddressUtil.java
@@ -5,12 +5,41 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.nio.charset.Charset;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.*;
 import com.csvreader.CsvReader;
 import com.csvreader.CsvWriter;
+
+import ezvcard.io.text.VCardReader;
+import ezvcard.property.Address;
+import ezvcard.property.Birthday;
+import ezvcard.property.Email;
+import ezvcard.property.FormattedName;
+import ezvcard.property.Telephone;
+import net.sourceforge.cardme.io.CompatibilityMode;
+import net.sourceforge.cardme.io.VCardWriter;
+import net.sourceforge.cardme.util.Util;
+import net.sourceforge.cardme.vcard.VCardImpl;
+import net.sourceforge.cardme.vcard.arch.EncodingType;
+import net.sourceforge.cardme.vcard.arch.ParameterTypeStyle;
+import net.sourceforge.cardme.vcard.arch.VCardVersion;
+import net.sourceforge.cardme.vcard.types.AdrType;
+import net.sourceforge.cardme.vcard.types.BDayType;
+import net.sourceforge.cardme.vcard.types.EmailType;
+import net.sourceforge.cardme.vcard.types.ExtendedType;
+import net.sourceforge.cardme.vcard.types.FNType;
+import net.sourceforge.cardme.vcard.types.NoteType;
+import net.sourceforge.cardme.vcard.types.PhotoType;
+import net.sourceforge.cardme.vcard.types.TelType;
+import net.sourceforge.cardme.vcard.types.media.ImageMediaType;
+import net.sourceforge.cardme.vcard.types.params.AdrParamType;
+import net.sourceforge.cardme.vcard.types.params.TelParamType;
 
 
 public class AddressUtil {
@@ -59,8 +88,6 @@ public class AddressUtil {
 		String f = fi.getAbsolutePath() + "\\通讯录.txt";
 		try {
 			FileWriter out = new FileWriter(f);
-//			String[][] data = new String[pt.size()][];
-//			int i = 0;
 			for (PeopleTable pttemp : pt) {
 				out.write("name:" + pttemp.getName()+"\n");
 				out.write("telephone:"+pttemp.getTelephone()+"\n");
@@ -316,6 +343,181 @@ public class AddressUtil {
 //			}
 		} catch (Exception e) {
 			System.out.println("导入失败");
+		}
+	}
+	public void vcardread() {
+		JFileChooser fd = new JFileChooser();
+		fd.setFileSelectionMode(JFileChooser.FILES_ONLY);
+		fd.setFileFilter(new FileFilter() {
+
+			@Override
+			public String getDescription() {
+				// TODO Auto-generated method stub
+				return ".vcf";
+			}
+
+			@Override
+			public boolean accept(File f) {
+				if (f.getName().endsWith(".vcf") || f.isDirectory()) {
+					return true;
+				} else {
+					return false;
+				}
+			}
+		});
+		fd.showOpenDialog(null);
+		File file = new File(fd.getSelectedFile().getAbsolutePath());
+		try {
+			VCardReader reader = new VCardReader(file);
+			ezvcard.VCard vcard;
+			while ((vcard = reader.readNext()) != null) {
+				PeopleTable pttemp = new PeopleTable();
+				// 姓名
+				FormattedName fn = vcard.getFormattedName();
+				pttemp.setName(fn.getValue());
+				// 家庭电话
+				Telephone tel = vcard.getTelephoneNumbers().get(0);
+				pttemp.setTelephone(tel.getText());
+				// 手机
+				Telephone phone = vcard.getTelephoneNumbers().get(1);
+				pttemp.setPhone(phone.getText());
+				// 电子邮箱
+				Email email = vcard.getEmails().get(0);
+				pttemp.setEmail(email.getValue());
+				// 生日
+				DateFormat df = new SimpleDateFormat("yyyy/MM/dd");
+				Birthday bday = vcard.getBirthday();
+				Date date = bday.getDate();
+				date.setDate(date.getDate() - 1);
+				String birthday = df.format(date);
+				pttemp.setBirthday(birthday);
+				// 工作地址
+				Address addr = vcard.getAddresses().get(0);
+				pttemp.setWorkplace(addr.getStreetAddress());
+				// 家庭地址
+				Address homeaddr = vcard.getAddresses().get(1);
+				pttemp.setHomeaddres(homeaddr.getStreetAddress());
+				// 邮编
+				pttemp.setPostcode(homeaddr.getPostalCode());
+				// 所属组
+				String tempstr = vcard.getExtendedProperties().get(1).getValue();
+				String[] group = new String[100];
+				if (tempstr != "") {
+					tempstr = tempstr.substring(1, tempstr.length() - 1);
+					group = tempstr.split(",");
+				} else {
+					group = null;
+				}
+				for (int i = 0; i < group.length - 1; i++) {
+					group[i] = group[i].substring(0, group[i].length() - 1);
+				}
+				pttemp.setGroup(group);
+				// 图片位置
+				pttemp.setPhotopath(vcard.getExtendedProperties().get(0).getValue());
+				// 备注
+				pttemp.setNote(vcard.getNotes().get(0).getValue());
+				pt.add(pttemp);
+			}
+			reader.close();
+			System.out.println("vcf导入成功");
+		} catch (Exception e) {
+			// TODO: handle exception
+			System.out.println("error");
+		}
+	}
+	public void vcardwrite() {
+		try {
+			JFileChooser jf = new JFileChooser();
+			jf.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			jf.showDialog(null, null);
+			File fi = jf.getSelectedFile();
+			String f = fi.getAbsolutePath() + "\\通讯录.vcf";
+			VCardWriter writer = new VCardWriter(VCardVersion.V3_0, CompatibilityMode.RFC2426);// 用户把VCard转换为字符
+			FileWriter fw = new FileWriter(new File(f));// 把VCard数据（字符）写入文件
+			
+			String[] str = new String[pt.size()+1];
+			int i = 0;
+			for (PeopleTable pttemp : pt) {
+				VCardImpl vc = new VCardImpl();// 创建一个名片
+				// 名字
+				vc.setFN(new FNType(pttemp.getName()));
+				// 家庭电话
+				TelType hometel = new TelType();
+				hometel.setCharset("UTF-8");
+				hometel.setTelephone(pttemp.getTelephone());
+				hometel.addParam(TelParamType.HOME);
+				hometel.setParameterTypeStyle(ParameterTypeStyle.PARAMETER_VALUE_LIST);
+				vc.addTel(hometel);
+				// 手机
+				TelType phone = new TelType();
+				phone.setCharset("UTF-8");
+				phone.setTelephone(pttemp.getPhone());
+				phone.addParam(TelParamType.CELL);
+				phone.setParameterTypeStyle(ParameterTypeStyle.PARAMETER_VALUE_LIST);
+				vc.addTel(phone);
+				// 电子邮箱
+				vc.addEmail(new EmailType(pttemp.getEmail()));
+				// 生日
+				Calendar birthday = Calendar.getInstance();
+				birthday.clear();
+				birthday.set(Calendar.YEAR, 2000);
+				birthday.set(Calendar.MONTH, 10 - 1);
+				birthday.set(Calendar.DATE, 1 + 1);
+				vc.setBDay(new BDayType(birthday));
+				// 照片
+				ExtendedType xphotopath = new ExtendedType("X-Photopath", pttemp.getPhotopath());
+				vc.addExtendedType(xphotopath);
+
+				// 工作单位
+				AdrType address2 = new AdrType();
+				address2.setCharset("UTF-8");
+				address2.setExtendedAddress("");
+				address2.setStreetAddress(pttemp.getWorkplace());
+				address2.addParam(AdrParamType.WORK);
+				vc.addAdr(address2);
+				// 家庭住址
+				AdrType address1 = new AdrType();
+				address1.setCharset("UTF-8");
+				address1.setExtendedAddress("");
+				address1.setStreetAddress(pttemp.getHomeaddres());
+				address1.setPostalCode("123456");// 邮编
+				address1.addParam(AdrParamType.HOME);
+				vc.addAdr(address1);
+
+				// 所属组
+				// 把它变成[数据,数据,数据]的string，再存入
+				String[] grouptemp = pttemp.getGroup();
+				String group = new String();
+				group = "[";
+				for (int temp = 0; temp < grouptemp.length; temp++) {
+					if (temp == 0) {
+						group = group + grouptemp[temp];
+					} else {
+						group = group + "," + grouptemp[temp];
+					}
+				}
+				group = group + "]";
+				ExtendedType xGroup = new ExtendedType("X-Group", group);
+				vc.addExtendedType(xGroup);
+
+				// 备注
+				NoteType note = new NoteType();
+				note.setNote(pttemp.getNote());
+				vc.addNote(note);
+				writer.setVCard(vc);
+				str[i] = writer.buildVCardString();// 把名片对象转化为字符
+				i++;
+			}
+			String vcString = new String();
+			for (int j = 0; j < str.length-1; j++) {
+				vcString = vcString + str[j];
+			}
+			fw.append(vcString);// 写入文件
+			fw.flush();
+			fw.close();
+			System.out.println("vcf导出成功");
+		} catch (Exception e) {
+			System.out.println("vcf导出失败");
 		}
 	}
 }
